8daf115d97d8ea39ffe0fcad686f777af4ac76e0
Jared <jaredhappy@gmail.com>
2019-01-16 13:03:04 -0800

    I accedently removed the website

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+
+backgroundChange = false;
+
 function changeBackground(color){
+    backgroundChange = true;
     document.body.style.backgroundColor = color;
+}
+
+function changeBackgroundBack(){
+    backgroundChange = false;
+    document.body.style.backgroundColor = '#ffe6ff';
 }
