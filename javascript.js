d5399a753df3615f75c17d09e6675b8482d33bda
Jared <jaredhappy@gmail.com>
2019-01-16 13:03:09 -0800

    Revert "I accedently removed the website"
    This reverts commit 8daf115d97d8ea39ffe0fcad686f777af4ac76e0.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-
-backgroundChange = false;
-
 function changeBackground(color){
-    backgroundChange = true;
     document.body.style.backgroundColor = color;
-}
-
-function changeBackgroundBack(){
-    backgroundChange = false;
-    document.body.style.backgroundColor = '#ffe6ff';
 }
